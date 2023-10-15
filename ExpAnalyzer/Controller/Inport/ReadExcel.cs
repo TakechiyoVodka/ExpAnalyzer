a29bb09b08bbf4f6b5b95a16baa76ad04514babb
Takehiro Somekawa <takehiro103368@gmail.com>
2023-10-15 20:46:51 +0900

    [fix] Excelからホールデータ読み込み機能修正

--- ExpAnalyzer/Controller/Inport/ReadExcel.cs
+++ ExpAnalyzer/Controller/Inport/ReadExcel.cs
@@ -12,11 +12,18 @@ using System.Diagnostics.Eventing.Reader;
 using System.Windows.Media.Animation;
 using System.Text.RegularExpressions;
 using System.ComponentModel;
+using System.Net.NetworkInformation;
 
 namespace ExpAnalyzer.Controller.Inport
 {
     internal class ClassReadExcel
     {
+        /// <summary>
+        /// 定数定義
+        /// </summary>
+        public const int DEFAULT_COLUMN = 2;
+        public const int SKIP_CELL_COUNT = 103;
+
         /// <summary>
         /// データ種別定義
         /// </summary>
@@ -64,87 +71,53 @@ namespace ExpAnalyzer.Controller.Inport
                         for (int i = 1; i <= Worksheet.Rows.EndRow; i++)
                         {
                             ClassUnitData UnitData = new ClassUnitData();
-                            bool endUnitFlg = false;
 
-                            for (int j = 2; j <= Worksheet.Columns.EndColumn + 1; j++)
+                            switch (Convert.ToString(Worksheet.Cells[i, DEFAULT_COLUMN].Value))
                             {
-                                if (j == 2 && Convert.ToString(Worksheet.Cells[i, j].Value) != "")
-                                {
-                                    switch (Convert.ToString(Worksheet.Cells[i, j].Value))
+                                //機種名
+                                case MODEL_NAME:
+
+                                    if (Convert.ToString(Worksheet.Cells[i, DEFAULT_COLUMN + 1].Value) == "")
                                     {
-                                        //機種名
-                                        case MODEL_NAME:
-
-                                            if (Convert.ToString(Worksheet.Cells[i, j + 1].Value) == "")
-                                            {
-                                                throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(i, j + 1), ")の機種名の取得に失敗しました。"));
-                                            }
-                                            ModelData.ModelName = Convert.ToString(Worksheet.Cells[i, j + 1].Value);
-                                            break;
-
-                                        //設置台数
-                                        case INSTALL_NUMBER:
-
-                                            if (int.TryParse(Convert.ToString(Worksheet.Cells[i, j + 1].Value), out int installNum) != true)
-                                            {
-                                                throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(i, j + 1), ")の設置台数の取得に失敗しました。"));
-                                            }
-                                            ModelData.InstallNum = installNum;
-                                            break;
-
-                                        //台番号
-                                        default:
-
-                                            if (Regex.IsMatch(Convert.ToString(Worksheet.Cells[i, j].Value), "^\\d+$") != true)
-                                            {
-                                                throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(i, j), ")の台番号の取得に失敗しました。"));
-                                            }
-                                            UnitData.UnitNum = Convert.ToString(Worksheet.Cells[i, j].Value);
-                                            unitCount++;
-                                            continue;
+                                        throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(i, DEFAULT_COLUMN + 1), ")の機種名の取得に失敗しました。"));
                                     }
-                                }
-                                else
-                                {
-                                    if (DateTime.TryParse(Convert.ToString(Worksheet.Cells[i, j].Value), out DateTime dateTime) == true)
+                                    ModelData.ModelName = Convert.ToString(Worksheet.Cells[i, DEFAULT_COLUMN + 1].Value);
+                                    break;
+
+                                //設置台数
+                                case INSTALL_NUMBER:
+
+                                    if (int.TryParse(Convert.ToString(Worksheet.Cells[i, DEFAULT_COLUMN + 1].Value), out int installNum) != true)
                                     {
+                                        throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(i, DEFAULT_COLUMN + 1), ")の設置台数の取得に失敗しました。"));
+                                    }
+                                    ModelData.InstallNum = installNum;
+                                    break;
+
+                                //台番号
+                                default:
+
+                                    if (Convert.ToString(Worksheet.Cells[i, DEFAULT_COLUMN].Value) != "")
+                                    {
+                                        if (Regex.IsMatch(Convert.ToString(Worksheet.Cells[i, DEFAULT_COLUMN].Value), "^\\d+$") != true)
+                                        {
+                                            throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(i, DEFAULT_COLUMN), ")の台番号の取得に失敗しました。"));
+                                        }
+                                        UnitData.UnitNum = Convert.ToString(Worksheet.Cells[i, DEFAULT_COLUMN].Value);
+
                                         //回転数と大当りステータスの取得
-                                        ClassDailyData DailyData = GetRotateCountAndHitStatus(Worksheet, i, j, dateTime);
+                                        UnitData.DailyDataList = GetRotateCountAndHitStatus(Worksheet, i, DEFAULT_COLUMN);
 
-                                        UnitData.DailyDataList.Add(DailyData);
-                                        j++;
-                                        continue;
-                                    }
-                                    else
-                                    {
-                                        if (Convert.ToString(Worksheet.Cells[i + 1, j].Value) == RTATE_COUNT
-                                            || Convert.ToString(Worksheet.Cells[i + 1, j + 1].Value) == HIT_STATUS)
-                                        {
-                                            throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(i, j), ")の日付情報の取得に失敗しました。"));
-                                        }
-                                        else
-                                        {
-                                            if (UnitData.UnitNum != null && UnitData.DailyDataList.Count != 0)
-                                            {
-                                                //1台毎の台データをリストへ追加
-                                                ModelData.UnitDataList.Add(UnitData);
+                                        //1台毎の台データをリストへ追加
+                                        ModelData.UnitDataList.Add(UnitData);
+                                        unitCount++;
 
-                                                //最終台のデータ取得が完了するまで探索
-                                                if (ModelData.InstallNum - unitCount != 0)
-                                                {
-                                                    i += 103;
-                                                }
-                                                else
-                                                {
-                                                    endUnitFlg = true;
-                                                }
-                                            }
-                                            break;
-                                        }
+                                        i += SKIP_CELL_COUNT;
                                     }
-                                }
+                                    break;
                             }
-                            if (endUnitFlg == true)
+                            //全ての台データ取得が完了したら探索終了
+                            if (ModelData.InstallNum != 0 && unitCount == ModelData.InstallNum)
                             {
                                 break;
                             }
@@ -155,102 +128,107 @@ namespace ExpAnalyzer.Controller.Inport
             }
             return HallData;
         }
+
         /// <summary>
         /// 回転数と大当りステータスの取得
         /// </summary>
-        private static ClassDailyData GetRotateCountAndHitStatus(ExcelWorksheet Worksheet, int row, int column, DateTime dateTime)
+        private static List<ClassDailyData> GetRotateCountAndHitStatus(ExcelWorksheet Worksheet, int defRow, int defColumn)
         {
-            ClassDailyData DailyData = new ClassDailyData();
-
-            //日付
-            DailyData.Date = dateTime;
-
             //Excel共通関数
             ClassExcelCommonFunction ExcelComFunc = new ClassExcelCommonFunction();
+            List<ClassDailyData> DayliDataList = new List<ClassDailyData>();
 
-            if (Convert.ToString(Worksheet.Cells[row + 1, column].Value) == RTATE_COUNT
-                && Convert.ToString(Worksheet.Cells[row + 1, column + 1].Value) == HIT_STATUS)
+            for (int column = defColumn + 1; column < Worksheet.Columns.EndColumn; column++)
             {
-                ClassHistoryData HistoryData;
-                bool convRotateCntResult = false;
-                bool convHitStatusResult = false;
-                int rotateCount = 0;
-                int hitStatus = 0;
+                ClassDailyData DailyData = new ClassDailyData();
 
-                row += 2;
-
-                if (Convert.ToString(Worksheet.Cells[row, column + 1].Value) != "-")
+                if (DateTime.TryParse(Convert.ToString(Worksheet.Cells[defRow, column].Value), out DateTime dateTime) == true)
                 {
-                    //ステータス(0)でない場合は探索
-                    while (int.Parse(Convert.ToString(Worksheet.Cells[row, column + 1].Value)) != 0)
-                    {
-                        HistoryData = new ClassHistoryData();
-                        convRotateCntResult = int.TryParse(Convert.ToString(Worksheet.Cells[row, column].Value), out rotateCount);
-                        convHitStatusResult = int.TryParse(Convert.ToString(Worksheet.Cells[row, column + 1].Value), out hitStatus);
+                    //日付
+                    DailyData.Date = dateTime;
 
-                        if (convRotateCntResult == true)
+                    if (Convert.ToString(Worksheet.Cells[defRow + 1, column].Value) == RTATE_COUNT
+                        && Convert.ToString(Worksheet.Cells[defRow + 1, column + 1].Value) == HIT_STATUS)
+                    {
+                        int row = defRow + 2;
+
+                        if (Convert.ToString(Worksheet.Cells[row, column].Value) == "-"
+                            || Convert.ToString(Worksheet.Cells[row, column + 1].Value) == "-")
                         {
+                            //定休日または故障台
+                            ClassHistoryData HistoryData = new ClassHistoryData();
+                            HistoryData.RotateCount = -1;
+                            HistoryData.HitStatus = -1;
+                            DailyData.HistoryDataList.Add(HistoryData);
+                        }
+                        else
+                        {
+                            ClassHistoryData HistoryData = new ClassHistoryData();
+                            int rotateCount = 0;
+                            int hitStatus = 0;
+
+                            //ステータス(0)でない場合は探索
+                            while (int.Parse(Convert.ToString(Worksheet.Cells[row, column + 1].Value)) != 0)
+                            {
+                                HistoryData = new ClassHistoryData();
+
+                                //回転数
+                                if (int.TryParse(Convert.ToString(Worksheet.Cells[row, column].Value), out rotateCount) != true)
+                                {
+                                    throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(row, column), ")の回転数の取得に失敗しました。"));
+                                }
+                                HistoryData.RotateCount = rotateCount;
+
+                                //ステータス
+                                if (int.TryParse(Convert.ToString(Worksheet.Cells[row, column + 1].Value), out hitStatus) != true)
+                                {
+                                    throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(row, column + 1), ")のステータスの取得に失敗しました。"));
+                                }
+                                HistoryData.HitStatus = hitStatus;
+
+                                DailyData.HistoryDataList.Add(HistoryData);
+                                row++;
+                            }
+                            //ステータス(0)を1行だけ取得 (残スタート回転数)
+                            HistoryData = new ClassHistoryData();
+
                             //回転数
+                            if (int.TryParse(Convert.ToString(Worksheet.Cells[row, column].Value), out rotateCount) != true)
+                            {
+                                throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(row, column), ")の回転数の取得に失敗しました。"));
+                            }
                             HistoryData.RotateCount = rotateCount;
-                        }
-                        else
-                        {
-                            throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(row, column), ")の回転数の取得に失敗しました。"));
-                        }
 
-                        if (convHitStatusResult == true)
-                        {
                             //ステータス
+                            if (int.TryParse(Convert.ToString(Worksheet.Cells[row, column + 1].Value), out hitStatus) != true)
+                            {
+                                throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(row, column + 1), ")のステータスの取得に失敗しました。"));
+                            }
                             HistoryData.HitStatus = hitStatus;
+
+                            DailyData.HistoryDataList.Add(HistoryData);
+                        }
+                        DayliDataList.Add(DailyData);
+                        column++;
+                    }
+                    else
+                    {
+                        if (Convert.ToString(Worksheet.Cells[defRow + 1, column].Value) != RTATE_COUNT)
+                        {
+                            throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(defRow + 1, column), ")の記載が不正です。"));
                         }
                         else
                         {
-                            throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(row, column + 1), ")のステータスの取得に失敗しました。"));
+                            throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(defRow + 1, column + 1), ")の記載が不正です。"));
                         }
-                        DailyData.HistoryDataList.Add(HistoryData);
-                        row++;
                     }
-                    HistoryData = new ClassHistoryData();
-
-                    //ステータス(0)を1行だけ取得
-                    convRotateCntResult = int.TryParse(Convert.ToString(Worksheet.Cells[row, column].Value), out rotateCount);
-                    convHitStatusResult = int.TryParse(Convert.ToString(Worksheet.Cells[row, column + 1].Value), out hitStatus);
-
-                    if (convRotateCntResult == true)
-                    {
-                        //回転数
-                        HistoryData.RotateCount = rotateCount;
-                    }
-                    else
-                    {
-                        throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(row, column), ")の回転数の取得に失敗しました。"));
-                    }
-
-                    if (convHitStatusResult == true)
-                    {
-                        //ステータス
-                        HistoryData.HitStatus = hitStatus;
-                    }
-                    else
-                    {
-                        throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(row, column + 1), ")のステータスの取得に失敗しました。"));
-                    }
-                    DailyData.HistoryDataList.Add(HistoryData);
                 }
                 else
                 {
-                    //定休日または故障台
-                    HistoryData = new ClassHistoryData();
-                    HistoryData.RotateCount = -1;
-                    HistoryData.HitStatus = -1;
-                    DailyData.HistoryDataList.Add(HistoryData);
+                    throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(defRow, column), ")の日付情報の取得に失敗しました。"));
                 }
             }
-            else
-            {
-                throw new Exception(string.Concat("セル(", ExcelComFunc.ConvertNumToRange(row, column), ")の値が不正です。"));
-            }
-            return DailyData;
+            return DayliDataList;
         }
     }
 }
