e07508843a05191d94c7676b899058cf68a6a039
Takehiro Somekawa <takehiro103368@gmail.com>
2023-12-12 00:28:12 +0900

    [fix] 機種スペックデータファイル読込み機能修正
    確率データのデータ型修正

--- ExpAnalyzer/Controller/Inport/InportModelSpecDataFile.cs
+++ ExpAnalyzer/Controller/Inport/InportModelSpecDataFile.cs
@@ -35,21 +35,20 @@ namespace ExpAnalyzer.Controller.Inport
 
             using (StreamReader sr = new StreamReader(InportInitFilePath, System.Text.Encoding.Default))
             {
-                ClassModelSpecData ModelDetailsInfo = new ClassModelSpecData();
+                ClassModelSpecData ModelSpecData = new ClassModelSpecData();
                 int modelNum = 0;
                 bool readFlg = false;
 
                 while (sr.EndOfStream == false)
                 {
                     string readLine = sr.ReadLine();
-                    int resultValue = 0;
 
                     //"■Model-"がヒットした行から読込み開始
                     if (Regex.Match(readLine, @"^■Model-\d+").Success == true)
                     {
                         string[] splitModelNum = readLine.Split('-');
 
-                        if(int.TryParse(splitModelNum[1], out resultValue) != true)
+                        if(int.TryParse(splitModelNum[1], out int resultValue) != true)
                         {
                             throw new Exception(string.Concat("■Model-(Number)の記載が不正です。"));
                         }
@@ -62,94 +61,97 @@ namespace ExpAnalyzer.Controller.Inport
                         if (readFlg == true)
                         {
                             string[] splitReadLine = readLine.Split(':');
+                            int resultCountValue = 0;
+                            double resultProbValue = 0;
+
 
                             switch (splitReadLine[0])
                             {
                                 //機種名
                                 case MODEL_NAME:
 
-                                    ModelDetailsInfo.ModelName = splitReadLine[1];
+                                    ModelSpecData.ModelName = splitReadLine[1];
                                     break;
 
                                 //初当り確率
                                 case FIRSTHIT_PROB:
 
-                                    if (int.TryParse(splitReadLine[1], out resultValue) != true)
+                                    if (double.TryParse(splitReadLine[1], out resultProbValue) != true)
                                     {
                                         throw new Exception(string.Concat("■Model-", modelNum, "の初当り確率の取得に失敗しました。"));
                                     }
-                                    ModelDetailsInfo.FirstHitProb = resultValue;
+                                    ModelSpecData.FirstHitProb = resultProbValue;
                                     break;
 
                                 //確変当り確率
                                 case PROBVARHIT_PROB:
 
-                                    if (int.TryParse(splitReadLine[1], out resultValue) != true)
+                                    if (double.TryParse(splitReadLine[1], out resultProbValue) != true)
                                     {
                                         throw new Exception(string.Concat("■Model-", modelNum, "の確変当り確率の取得に失敗しました。"));
                                     }
-                                    ModelDetailsInfo.ProbVarHitProb = resultValue;
+                                    ModelSpecData.ProbVarHitProb = resultProbValue;
                                     break;
 
                                 //確変突入率
                                 case PROBVARHIT_RASHRATE:
 
-                                    if (int.TryParse(splitReadLine[1], out resultValue) != true)
+                                    if (double.TryParse(splitReadLine[1], out resultProbValue) != true)
                                     {
                                         throw new Exception(string.Concat("■Model-", modelNum, "の確変突入率の取得に失敗しました。"));
                                     }
-                                    ModelDetailsInfo.ProbVarHitRashRate = resultValue;
+                                    ModelSpecData.ProbVarHitRashRate = resultProbValue;
                                     break;
 
                                 //確変継続率
                                 case PROBVARHIT_PERSISRATE:
 
-                                    if (int.TryParse(splitReadLine[1], out resultValue) != true)
+                                    if (double.TryParse(splitReadLine[1], out resultProbValue) != true)
                                     {
                                         throw new Exception(string.Concat("■Model-", modelNum, "の確変継続率の取得に失敗しました。"));
                                     }
-                                    ModelDetailsInfo.ProbVarHitPersisRate = resultValue;
+                                    ModelSpecData.ProbVarHitPersisRate = resultProbValue;
                                     break;
 
                                 //ST
                                 case SPECIAL_TIME:
 
-                                    if (int.TryParse(splitReadLine[1], out resultValue) != true)
+                                    if (int.TryParse(splitReadLine[1], out resultCountValue) != true)
                                     {
                                         throw new Exception(string.Concat("■Model-", modelNum, "のSTの取得に失敗しました。"));
                                     }
-                                    ModelDetailsInfo.SpecialTime = resultValue;
+                                    ModelSpecData.SpecialTime = resultCountValue;
                                     break;
 
                                 //時短回数
                                 case SAVING_TIME:
 
-                                    if (int.TryParse(splitReadLine[1], out resultValue) != true)
+                                    if (int.TryParse(splitReadLine[1], out resultCountValue) != true)
                                     {
                                         throw new Exception(string.Concat("■Model-", modelNum, "の時短回数の取得に失敗しました。"));
                                     }
-                                    ModelDetailsInfo.SavingTime = resultValue;
+                                    ModelSpecData.SavingTime = resultCountValue;
                                     break;
 
                                 //CT
                                 case C_TIME:
 
-                                    if (int.TryParse(splitReadLine[1], out resultValue) != true)
+                                    if (int.TryParse(splitReadLine[1], out resultCountValue) != true)
                                     {
                                         throw new Exception(string.Concat("■Model-", modelNum, "のCTの取得に失敗しました。"));
                                     }
-                                    ModelDetailsInfo.CTime = resultValue;
+                                    ModelSpecData.CTime = resultCountValue;
                                     break;
                             }
 
                             //全ての機種詳細情報の読込み完了後リストへ追加
-                            if (ModelDetailsInfo.ModelName != null && ModelDetailsInfo.FirstHitProb != 0
-                                && ModelDetailsInfo.ProbVarHitProb != 0 && ModelDetailsInfo.ProbVarHitRashRate != 0
-                                && ModelDetailsInfo.ProbVarHitPersisRate != 0 && ModelDetailsInfo.SpecialTime != 0
-                                && ModelDetailsInfo.SavingTime != 0 && ModelDetailsInfo.CTime != 0)
+                            if (ModelSpecData.ModelName != null && ModelSpecData.FirstHitProb != 0
+                                && ModelSpecData.ProbVarHitProb != 0 && ModelSpecData.ProbVarHitRashRate != 0
+                                && ModelSpecData.ProbVarHitPersisRate != 0 && ModelSpecData.SpecialTime != 0
+                                && ModelSpecData.SavingTime != 0 && ModelSpecData.CTime != 0)
                             {
-                                ModelSpecDataList.Add(ModelDetailsInfo);
-                                ModelDetailsInfo = new ClassModelSpecData();
+                                ModelSpecDataList.Add(ModelSpecData);
+                                ModelSpecData = new ClassModelSpecData();
                                 readFlg = false;
                             }
                         }
